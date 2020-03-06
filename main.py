76887f59f8832754c3b1919dd2bd922652ffb36c
dannobot <danqwe1996@gmail.com>
2020-03-06 17:58:29 +0200

    commit:0.0.1

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from PyQt5.QtWidgets import *
-from PyQt5 import uic
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-
+from PyQt5 import QtWidgets, QtGui, QtCore
+from mainUI import Ui_MainWindow
 import sys
 
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.label.setText("None")
+        self.ui.button.clicked.connect(self.btnClick)
+    def btnClick(self):
+        lable_text = self.ui.label.text()
+        if lable_text == "None":
+            self.ui.label.setText("Clicked")
+        else:
+            self.ui.label.setText("None")
 
-app = QApplication([])
-win = uic.loadUi('main.ui')
 
-win.show()
-sys.exit(app.exit())
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    app_main = MyWindow()
+    app_main.show()
+    sys.exit(app.exec_())
---
+++ mainUI.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 604)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.button = QtWidgets.QPushButton(self.centralwidget)
+        self.button.setGeometry(QtCore.QRect(250, 430, 241, 111))
+        self.button.setObjectName("button")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(260, 170, 231, 101))
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionAdd_file = QtWidgets.QAction(MainWindow)
+        self.actionAdd_file.setObjectName("actionAdd_file")
+        self.menuFile.addAction(self.actionAdd_file)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.button.setText(_translate("MainWindow", "Push Me"))
+        self.label.setText(_translate("MainWindow", "Window"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.actionAdd_file.setText(_translate("MainWindow", "Add file"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
